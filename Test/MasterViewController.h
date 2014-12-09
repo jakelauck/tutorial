5dc3cb856e91608ebe3287a410164c521a6fbb2c
FWD <FWD@FWDs-MacBook-Pro.local>
2014-12-09 11:26:35 +0800

    Initial commit

---
+++ Test/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Test
+//
+//  Created by FWD on 5/12/14.
+//  Copyright (c) 2014 FWD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
---
+++ Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test
+//
+//  Created by FWD on 5/12/14.
+//  Copyright (c) 2014 FWD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
